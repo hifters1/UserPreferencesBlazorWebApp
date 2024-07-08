bb38d05f7781eaad67f043ce25a937515698e1b8
hifters1 <dpjorvis@netzero.net>
2024-07-07 23:10:37 -0500

    User page updates.

--- AppService.cs
+++ AppService.cs
@@ -1,6 +1,91 @@
-﻿namespace UserPreferencesBlazorWebApp
+﻿using Microsoft.EntityFrameworkCore;
+using UserPreferencesBlazorWebApp.Data;
+using UserPreferencesBlazorWebApp.Models;
+
+
+namespace UserPreferencesBlazorWebApp
 {
 	public class AppService
 	{
+		private readonly IDbContextFactory<AppDbContext> _dbContextFactory;
+
+		public AppService(IDbContextFactory<AppDbContext> dbContextFactory)
+		{
+			_dbContextFactory = dbContextFactory;
+		}
+
+		//Preference methods
+		//GetAllPreferences
+		//
+		public async Task<IQueryable<User>> GetAllUserPreferencesAsyc()
+		{
+			using var context = await _dbContextFactory.CreateDbContextAsync();
+			return (IQueryable<User>)await context.Users.Include(_ => _.PreferenceList).ToListAsync();
+		}
+
+
+		//User methods
+		// GetAllUsersAsyc - get all users from db to display on the UserList page
+		// GetUserByIdAsync - get a specific user by their id
+		// UpdateUserAsync - update a users data from overlay
+		// DeleteUserAsync - remove the user from the db
+
+		public async Task<IEnumerable<User>> GetAllUsersAsyc()
+		{
+			using var context = await _dbContextFactory.CreateDbContextAsync();
+			return await context.Users.ToListAsync();
+		}
+
+		public async Task<User?> GetUserByIdAsync(int id)
+		{
+			using var context = await _dbContextFactory.CreateDbContextAsync();
+			return await context.Users.FirstOrDefaultAsync(u => u.Id == id);
+		}
+
+		public async Task<User?> UpdateUserAsync(User updatedUser)
+		{
+			using var context = await _dbContextFactory.CreateDbContextAsync();
+			var existingUser = await context.Users.FirstOrDefaultAsync(p => p.Id == updatedUser.Id);
+
+			if (existingUser is not null)
+			{
+				existingUser.FirstName = updatedUser.FirstName;
+				await context.SaveChangesAsync();
+				return existingUser;
+			}
+			return null;
+		}
+
+		public async Task DeleteUserAsync(int id)
+		{
+			using var context = await _dbContextFactory.CreateDbContextAsync();
+			var existingUser = await context.Users.FirstOrDefaultAsync(p => p.Id == id);
+
+			if (existingUser is not null)
+			{
+				context.Users.Remove(existingUser);
+				await context.SaveChangesAsync();
+			}
+		}
+
+		public async Task<User> AddUserAsync(User user)
+		{
+			//Load new user info into user table
+			using var context = await _dbContextFactory.CreateDbContextAsync();
+			context.Users.Add(user);
+			await context.SaveChangesAsync();
+
+			//Update UserPreference table with the just loaded user id
+			var newUserPref = new UserPreference();
+			newUserPref.PreferenceName = null;
+			newUserPref.UserId = user.Id;
+			context.UserPreferences.Add(newUserPref);
+			await context.SaveChangesAsync();
+			return user;
+		}
+
+		//UserPreference methods
+		//
+		//
 	}
 }
---
+++ Migrations/20240708022535_initialize.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UserPreferencesBlazorWebApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class initialize : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Preferences",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Preferences", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Phone = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserPreferences",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PreferenceName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserPreferences", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserPreferences_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserPreferences_UserId",
+                table: "UserPreferences",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Preferences");
+
+            migrationBuilder.DropTable(
+                name: "UserPreferences");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
